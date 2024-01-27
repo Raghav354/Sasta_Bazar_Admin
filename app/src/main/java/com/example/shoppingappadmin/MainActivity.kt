7c31c622f3f146d068ed876bf252257a26751856
Rajat Sharma <rajat200229@gmail.com>
2024-01-27 16:25:53 +0530

    Initial commit -> Categories option added

---
+++ app/src/main/java/com/example/shoppingappadmin/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.shoppingappadmin
+
+import android.app.Activity
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shoppingappadmin.databinding.ActivityMainBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.util.UUID
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    val productModel = ProductModel()
+    var categoryList = arrayOf("Dresses", "Tops", "JumpSuits", "Bottoms")
+
+    private val startForProfileImageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            if (resultCode == Activity.RESULT_OK) {
+                binding.rootLayout.setBackgroundColor(Color.LTGRAY)
+                binding.mainLayout.visibility = View.GONE
+                binding.spinKit.visibility = View.VISIBLE
+                //Image Uri will not be null for RESULT_OK
+                val fileUri = data?.data!!
+
+                //When data upload on firebase
+
+                Firebase.storage.reference.child("ProductImage/${UUID.randomUUID()}")
+                    .putFile(fileUri).addOnCompleteListener {
+
+                        if (it.isSuccessful) {
+
+                            it.result.storage.downloadUrl.addOnSuccessListener {
+                                productModel.imageUrl = it.toString()
+                                binding.productImage.setImageURI(fileUri)
+                                binding.mainLayout.visibility = View.VISIBLE
+                                binding.spinKit.visibility = View.GONE
+                                binding.rootLayout.setBackgroundColor(Color.WHITE)
+                            }
+
+                        }
+                    }
+
+
+            } else if (resultCode == ImagePicker.RESULT_ERROR) {
+                Toast.makeText(this, ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.productImage.setOnClickListener {
+            ImagePicker.with(this)
+                .crop()
+                .createIntent { intent ->
+                    startForProfileImageResult.launch(intent)
+                }
+        }
+        var arrayAdapter =
+            ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, categoryList)
+        binding.category.adapter = arrayAdapter
+
+        binding.category.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                productModel.category = categoryList.get(p2)
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+
+        }
+
+        binding.addProduct.setOnClickListener {
+            if (binding.productName.text.toString().isEmpty()) {
+                binding.productName.error = "Error:- Add product name"
+            } else if (binding.price.text.toString().isEmpty()) {
+                binding.price.error = "Error:- Add product price"
+            } else if (binding.disp.text.toString().isEmpty()) {
+                binding.disp.error = "Error:- Add product description"
+            } else if (binding.disp.text.toString().trim().length < 80) {
+                binding.disp.error = "Error:- Need more description."
+            } else if (productModel.imageUrl.isEmpty()) {
+                Toast.makeText(this@MainActivity, "Add image...!!", Toast.LENGTH_SHORT).show()
+            } else {
+                productModel.name = binding.productName.text.toString()
+                productModel.price = binding.price.text.toString().toDouble()
+                productModel.disp = binding.disp.text.toString()
+                Firebase.firestore.collection("Products").document(UUID.randomUUID().toString())
+                    .set(productModel).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Toast.makeText(this@MainActivity, "Product Added!!", Toast.LENGTH_SHORT)
+                                .show()
+                        } else {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Error in adding the product!!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shoppingappadmin/ProductModel.kt
@@ -0,0 +1,26 @@
+package com.example.shoppingappadmin
+
+class ProductModel {
+    var name: String = ""
+    var price: Double = -1.0
+    var disp: String = ""
+    var imageUrl: String = ""
+    var category: String = ""
+
+    constructor(name: String, price: Double, disp: String, imageUrl: String) {
+        this.name = name
+        this.price = price
+        this.disp = disp
+        this.imageUrl = imageUrl
+    }
+
+    constructor()
+
+    constructor(name: String, price: Double, disp: String, imageUrl: String, category: String) {
+        this.name = name
+        this.price = price
+        this.disp = disp
+        this.imageUrl = imageUrl
+        this.category = category
+    }
+}
